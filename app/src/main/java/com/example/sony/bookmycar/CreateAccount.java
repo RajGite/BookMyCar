62384c24f2def8b95af911aa8a6672837871c599
RajGite1998 <rajgite1998@gmail.com>
2017-08-05 18:09:39 +0530

    signup and signin

--- app/src/main/java/com/example/sony/bookmycar/CreateAccount.java
+++ app/src/main/java/com/example/sony/bookmycar/CreateAccount.java
@@ -30,12 +30,9 @@ import com.google.firebase.database.ValueEventListener;
 
 public class CreateAccount extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText editTextName;
-    private EditText editTextAddress;
+    private EditText editTextName,editTextAddress,editTextContact_no,editTextEmail,editTextPassword,editTextConfirmPassword;
     private RadioGroup rg;
     private RadioButton rb;
-    private EditText editTextEmail;
-    private EditText editTextPassword;
     private Button buttonSignup;
     private ProgressDialog progressDialog;
 
@@ -48,79 +45,104 @@ public class CreateAccount extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
-        firebaseAuth=FirebaseAuth.getInstance();
-        firebaseDatabase=FirebaseDatabase.getInstance();
-        dataBaseReference=firebaseDatabase.getReference();
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        dataBaseReference = firebaseDatabase.getReference();
 
-        progressDialog=new ProgressDialog(this);
+        progressDialog = new ProgressDialog(this);
 
-        editTextName= (EditText) findViewById(R.id.editTextName);
-        editTextAddress= (EditText) findViewById(R.id.editTextAddress);
-        //rg=(RadioGroup)findViewById(R.id.rg);
-        //rb= (RadioButton)findViewById(rg.getCheckedRadioButtonId());
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextAddress = (EditText) findViewById(R.id.editTextAddress);
+        editTextContact_no= (EditText) findViewById(R.id.editTextContact_no);
+
+        rg = (RadioGroup) findViewById(R.id.rg);
+
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        editTextConfirmPassword= (EditText) findViewById(R.id.editTextConfirmPassword);
+
         buttonSignup = (Button) findViewById(R.id.buttonSignup);
 
         buttonSignup.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        //String User= rb.getText().toString().trim();
-        final String name= editTextName.getText().toString().trim();
-        final String email = editTextEmail.getText().toString().trim();
-        String password = editTextPassword.getText().toString().trim();
-
-        if (TextUtils.isEmpty(email)) {
-            Toast.makeText(this, "Plese Enter Email", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if (TextUtils.isEmpty(password))
-        {
-            Toast.makeText(this, "Plese Enter Password", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        /* if all feilds are filled register user by transfering all details to firebase server// */
-       progressDialog.setMessage("Registering User ...");
-       progressDialog.show();
-
-        firebaseAuth.createUserWithEmailAndPassword(email,password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        progressDialog.dismiss();
-                        if(task.isSuccessful())
-                        {
-
-                            Toast.makeText(CreateAccount.this,"Registered Successfully",Toast.LENGTH_SHORT).show();
-                            //add details of user to firebase
-                            //if(User.equals("Passenger")) {
-                                //add details to passenger node in firebase
-                                UserInformation u=new UserInformation(name,email);
-                                pushData(u);
-                              /* finish();
-                                startActivity(new Intent(getApplicationContext(), Passenger.class));
-                            }
-                            else if(User.equals("Driver")){*/
-                                //add details to driver node in firebase
-                                finish();
-                                startActivity(new Intent(getApplicationContext(), Driver.class));
-                            //}
-
-                        }
-                        else
-                        {
-                            Toast.makeText(CreateAccount.this,"Couldn't Register.Please try again",Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                });
-
-    }
     public void pushData(UserInformation u)
     {
         dataBaseReference.child(firebaseAuth.getCurrentUser().getUid().toString()).setValue(u);
+    }
+    @Override
+    public void onClick(View v) {
+        if (v == buttonSignup) {
+
+            int radioButtonId = rg.getCheckedRadioButtonId();
+            rb = (RadioButton) rg.findViewById(radioButtonId);
+
+            final String User = (String) rb.getText();
+            Toast.makeText(this, User, Toast.LENGTH_SHORT).show();
+            final String name = editTextName.getText().toString().trim();
+            final String address = editTextAddress.getText().toString().trim();
+            final String contact_no= editTextContact_no.getText().toString();
+            final String email = editTextEmail.getText().toString().trim();
+            String password = editTextPassword.getText().toString().trim();
+            String confirmpassword= editTextConfirmPassword.getText().toString().trim();
+
+            if (TextUtils.isEmpty(name)) {
+                Toast.makeText(this, "Please Enter Your Name", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (TextUtils.isEmpty(address)) {
+                Toast.makeText(this, "Please Enter Address", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (TextUtils.isEmpty(contact_no)) {
+                Toast.makeText(this, "Please Enter Your Phone Number", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(this, "Please Enter Email", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(this, "Please Enter Password", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (TextUtils.isEmpty(confirmpassword)) {
+                Toast.makeText(this, "Please Confirm Your Password", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            /* if all feilds are filled register user by transfering all details to firebase server */
+            if(password.equals(confirmpassword)) {
+                progressDialog.setMessage("Registering User ...");
+                progressDialog.show();
+
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    progressDialog.dismiss();
+                                    Toast.makeText(CreateAccount.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                                    /*add details of user to firebase*/
+                                    if (User.equals("Passenger")) {
+                                        UserInformation u = new UserInformation(name, address, contact_no, User, email);
+                                        pushData(u);
+                                        finish();
+                                        startActivity(new Intent(getApplicationContext(), Passenger.class));
+                                    }
+                                    else if(User.equals("Driver")){
+                                        /*add details to driver node in firebase*/
+                                        UserInformation u = new UserInformation(name, address, contact_no, User, email);
+                                        pushData(u);
+                                        finish();
+                                        startActivity(new Intent(getApplicationContext(), Driver.class));
+                                    }
+                                }
+                                else {
+                                    Toast.makeText(CreateAccount.this, "Couldn't Register.Please try again", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        }
     }
 }
--- app/src/main/java/com/example/sony/bookmycar/UserInformation.java
+++ app/src/main/java/com/example/sony/bookmycar/UserInformation.java
@@ -4,17 +4,32 @@ package com.example.sony.bookmycar;
  * Created by SONY on 29-07-2017.
  */
 public class UserInformation {
-    private String name,email;
+    private String name,address,contact_no,user,email;
 
     UserInformation()
     {
 
     }
 
-    UserInformation(String name,String email)
+    UserInformation(String name,String address,String contact_no,String user,String email)
     {
         this.name=name;
+        this.address=address;
+        this.contact_no=contact_no;
+        this.user=user;
         this.email=email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getContact_no() {
+        return contact_no;
+    }
+
+    public String getUser() {
+        return user;
     }
 
     public String getEmail() {
