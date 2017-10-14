18c1a10c5c27574b5675953e93240a19bee894b1
RajGite1998 <rajgite1998@gmail.com>
2017-10-14 12:43:47 +0530

    Proper Activity is opened for Passenger and Driver after signin

--- app/src/main/java/com/example/sony/bookmycar/MainActivity.java
+++ app/src/main/java/com/example/sony/bookmycar/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.sony.bookmycar;
 
+import android.accounts.Account;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.provider.SyncStateContract;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +19,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -26,6 +34,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private ProgressDialog progressDialog;
     private FirebaseAuth firebaseAuth;
+    private DatabaseReference dataBaseReference;
+    private FirebaseDatabase firebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +43,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         firebaseAuth=FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        dataBaseReference = firebaseDatabase.getReference();
+
         if(firebaseAuth.getCurrentUser()!=null)
         {
             //start passenger or driver activity accordingly
@@ -53,34 +66,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         if(v==buttonSignin)
         {
+            progressDialog.setMessage("Signing in ...");
+            progressDialog.show();
+
             String email = editTextEmail.getText().toString().trim();
             String password = editTextPassword.getText().toString().trim();
 
             if (TextUtils.isEmpty(email)) {
-                Toast.makeText(this, "Plese Enter Email", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Please Enter Email", Toast.LENGTH_SHORT).show();
                 return;
             }
             if (TextUtils.isEmpty(password))
             {
-                Toast.makeText(this, "Plese Enter Password", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Please Enter Password", Toast.LENGTH_SHORT).show();
                 return;
             }
-            progressDialog.setMessage("Signing in ...");
-            progressDialog.show();
 
             firebaseAuth.signInWithEmailAndPassword(email,password)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
-                            progressDialog.dismiss();
                            if(task.isSuccessful())
                            {
-                               //start passenger or driver activity
-                               finish();
-                               startActivity(new Intent(getApplicationContext(),Passenger.class));
+                               /*Finds the signed in user node in firebase*/
+                               dataBaseReference.child(firebaseAuth.getCurrentUser().getUid().toString()).addListenerForSingleValueEvent(new ValueEventListener() {
+                                   @Override
+                                   public void onDataChange(DataSnapshot snapshot) {
+
+                                       try{
+                                           /*Extracts the user type from the node found*/
+                                           String user=(String)snapshot.child("user").getValue();
+
+                                           progressDialog.dismiss();
+
+                                           if(user.equals("Passenger")) {
+                                               startActivity(new Intent(getApplicationContext(), Passenger.class));
+                                               finish();
+                                           }
+                                           else if(user.equals("Driver")) {
+                                               startActivity(new Intent(getApplicationContext(), Driver.class));
+                                               finish();
+                                           }
+
+                                       } catch (Throwable e) {
+                                           System.out.println("Some Problem");
+                                           e.printStackTrace();
+                                       }
+                                   }
+                                   @Override public void onCancelled(DatabaseError error) { }
+                               });
                            }
                             else
                            {
+                               progressDialog.dismiss();
                                Toast.makeText(MainActivity.this,"Account not found.Please create an account",Toast.LENGTH_SHORT).show();
                            }
                         }
@@ -94,3 +132,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 }
+
